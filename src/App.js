82d476361802408a50cfc5ee386457645fe82b72
Marat <94042173+Crescent17@users.noreply.github.com>
2023-08-29 16:16:12 +0300

    add sorting by input, description and packed to list

--- src/App.js
+++ src/App.js
@@ -51,16 +51,30 @@ function Form({onAddItems}) {
 }
 
 function PackingList({items, onDeleteItem, onToggleItem}) {
+    const [sortBy, setSortBy] = useState('input');
+    let sortedItems;
+    if (sortBy === 'input') sortedItems = items;
+    if (sortBy === 'description') sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description))
+    if (sortBy === 'packed') sortedItems = items.slice().sort((a, b) => a.packed - b.packed)
+
     return <div className='list'>
         <ul>
-            {items.map(item => <Item key={item.id} item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem}/>)}
+            {sortedItems.map(item => <Item key={item.id} item={item} onDeleteItem={onDeleteItem}
+                                           onToggleItem={onToggleItem}/>)}
         </ul>
+        <div className="actions">
+            <select name="" id="" value={sortBy} onChange={e => setSortBy(e.target.value)}>
+                <option value="input">Sort by input order</option>
+                <option value="description">Sort by description</option>
+                <option value="packed">Sort by packed status</option>
+            </select>
+        </div>
     </div>
 }
 
 function Item({item, onDeleteItem, onToggleItem}) {
     return <li>
-        <input type="checkbox" value={item.packed} onChange={() => {onToggleItem(item.id)}}/>
+        <input type="checkbox" value={item.packed} onChange={() => onToggleItem(item.id)}/>
         <span style={item.packed ? {textDecoration: 'line-through'} : {}}>{item.quantity} {item.description}</span>
         <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
